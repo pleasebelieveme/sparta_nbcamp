82210635a57b0cb4462b4c8a6db5891d855d287d
pleasebelieveme <cain1227@naver.com>
2025-02-26 22:48:41 +0900

    스파르타 1주차 계산기과제  ui생성

---
+++ java-course/src/assignment/calculator/level1/Main.java
@@ -0,0 +1,20 @@
+package assignment.calculator.level1;
+
+import assignment.calculator.ui.Display;
+
+public class Main {
+	public static void main(String[] args) {
+		// 계산기 화면
+		Display display = new Display();
+		display.ui();
+		display.menu();
+
+		// 양의 정수(0 포함)를 입력받기
+
+		// 사칙연산 기호(➕,➖,✖️,➗)를 입력받기
+
+		// 위에서 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기
+
+		// 반복문을 사용하되, 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기
+	}
+}
---
+++ java-course/src/assignment/calculator/ui/Display.java
@@ -0,0 +1,19 @@
+package assignment.calculator.ui;
+public class Display {
+	public void ui() {
+		System.out.println("+---------------------------+");
+		System.out.println("| ////// Calculator /////// |");
+		System.out.println("+---------------------------+");
+		System.out.println("| [              1,264.45 ] |");
+		System.out.println("+---------------------------+");
+		System.out.println("| [ 7 ] [ 8 ] [ 9 ] [  *  ] |");
+		System.out.println("| [ 4 ] [ 5 ] [ 6 ] [  -  ] |");
+		System.out.println("| [ 1 ] [ 2 ] [ 3 ] [  +  ] |");
+		System.out.println("| [ 0 ] [ . ] [+/-] [  =  ] |");
+		System.out.println("+---------------------------+");
+	}
+	public void menu() {
+		System.out.println("1. 두개의 숫자 더하기");
+		System.out.println("0. 종료하기");
+	}
+}
